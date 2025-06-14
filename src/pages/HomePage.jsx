0288ba2ba1eb8fd9ea9efc9d2314c96f144052d5
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-06-14 19:52:05 -0400

    Style home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
+import styles from "../styles/HomePage.module.css";
+import { Link } from "react-router";
+
 const HomePage = () => {
   return (
-    <>
-      <h1>Welcome to eStore</h1>
-      <p>
-        Discover thousands of curated products — from everyday essentials to
-        one-of-a-kind finds. Delivered fast, priced right, and backed by
-        quality.
-      </p>
-      <button>Shop Now</button>
-    </>
+    <section className={styles.homePage}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>Welcome to eStore</h1>
+        <p className={styles.text}>
+          Discover thousands of curated products — from everyday essentials to
+          one-of-a-kind finds. Delivered fast, priced right, and backed by
+          quality.
+        </p>
+        <Link to="/shop">
+          <button className={styles.btn}>Shop Now</button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
