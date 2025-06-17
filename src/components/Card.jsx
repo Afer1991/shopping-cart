6fa5bc55c71061e224e51062ccd1d40c1fb83841
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-06-16 20:31:48 -0400

    Modify Card and ShopPage components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,30 @@
 import svg from "../assets/react.svg";
+import styles from "../styles/Card.module.css";
+import { useState } from "react";
 
-const Card = () => {
+const Card = ({ image = svg, name = "Name", price = "$10" }) => {
+  const [amount, setAmount] = useState(0);
+
   return (
-    <div>
-      <img src={svg} alt="logo" />
-      <h1>Name</h1>
-      <p>$10</p>
+    <div className={styles.card}>
+      <img src={image} alt="logo" />
+      <h1>{name}</h1>
+      <p>{price}</p>
       <form>
         <div>
-          <button type="button">-</button>
-          <input type="number" />
-          <button type="button">+</button>
+          <button
+            type="button"
+            onClick={() => setAmount((amount) => (amount > 1 ? amount - 1 : 0))}
+          >
+            -
+          </button>
+          <input type="text" value={amount} readOnly />
+          <button
+            type="button"
+            onClick={() => setAmount((amount) => amount + 1)}
+          >
+            +
+          </button>
         </div>
         <button>Add to Cart</button>
       </form>
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,43 @@
 import Card from "../components/Card.jsx";
+import styles from "../styles/ShopPage.module.css";
+import { useState, useEffect } from "react";
 
 const ShopPage = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        if (!response.ok) {
+          throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        setProducts(data);
+        return;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <Card></Card>
+      <h1>Products</h1>
+      <div className={styles.cards}>
+        {products.map((product) => {
+          <Card
+            key={product.id}
+            image={product.image}
+            name={product.title}
+            price={"$" + product.price}
+          />;
+        })}
+      </div>
     </>
   );
 };
