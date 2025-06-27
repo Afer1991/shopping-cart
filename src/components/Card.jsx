15b77ef360860dc67373c32e0be7c8e95c209bd5
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-06-27 00:53:37 -0400

    Adjust style for Card component and Shop Page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import styles from "../styles/Card.module.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ article, addCart }) => {
   const [amount, setAmount] = useState(0);
@@ -18,7 +20,7 @@ const Card = ({ article, addCart }) => {
             type="button"
             onClick={() => setAmount((amount) => (amount > 1 ? amount - 1 : 0))}
           >
-            -
+            <FontAwesomeIcon icon={faMinus} className={styles.minusIcon} />
           </button>
           <input className={styles.input} type="text" value={amount} readOnly />
           <button
@@ -26,7 +28,7 @@ const Card = ({ article, addCart }) => {
             type="button"
             onClick={() => setAmount((amount) => amount + 1)}
           >
-            +
+            <FontAwesomeIcon icon={faPlus} className={styles.plusIcon} />
           </button>
         </div>
         <button
